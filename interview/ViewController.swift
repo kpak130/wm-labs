8173da63f09b23a5785e3e6092a9a942d696ce35
Kisang Pak <kisangpak@gmail.com>
2017-07-08 19:11:56 -0700

    Initial Commit

---
+++ interview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interview
+//
+//  Created by Kisang Pak on 7/8/17.
+//  Copyright © 2017 Kisang Pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
